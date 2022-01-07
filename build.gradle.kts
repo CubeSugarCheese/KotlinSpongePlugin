5c41d390ae498be0f446ca59fad78c70bca1163f
方糖 <cubesugarcheese@qq.com>
2022-01-07 19:35:07 +0800

    Initialize api-9

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.model.PluginDependency
+
+plugins {
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "2.0.0"
+}
+
+group = "org.spongepowered"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sponge {
+    apiVersion("9.0.0-SNAPSHOT")
+    license("CHANGEME")
+    loader {
+        name(PluginLoaders.JAVA_PLAIN)
+        version("1.0")
+    }
+    plugin("example") {
+        displayName("Example")
+        entrypoint("org.spongepowered.example.Example")
+        description("Just testing things...")
+        links {
+            homepage("https://spongepowered.org")
+            source("https://spongepowered.org/source")
+            issues("https://spongepowered.org/issues")
+        }
+        contributor("Spongie") {
+            description("Lead Developer")
+        }
+        dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+    }
+}
+
+val javaTarget = 17
+java {
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+    if (JavaVersion.current() < JavaVersion.toVersion(javaTarget)) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(javaTarget))
+    }
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        release.set(javaTarget)
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
